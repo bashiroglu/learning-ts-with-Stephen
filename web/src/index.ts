8f1328f81b7bf0be04454316fa44e8dd46182599
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2020-08-24 07:12:09 +0400

    improve web project;

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,15 +1,23 @@
 import { User } from "./models/User";
 
-const user = new User({ age: 44, name: "Hasan" });
+const user = new User({ name: "Hasan", age: 55 });
+const user2 = new User({ name: "Hasan", age: 55 });
+// user.fetch();
+// user.set({ name: "Murad", age: 44 });
+console.log(user2.events === user.events);
 
-user.on("click", () => {
-  console.log("click");
-});
-user.on("click", () => {
-  console.log("click2");
-});
-user.on("change", () => {
-  console.log("change");
-});
+// user.save();
 
-user.trigger("click");
+// const user = new User({ age: 44, name: "Hasan", id: 1 });
+
+// user.on("click", () => {
+//   console.log("click");
+// });
+// user.on("click", () => {
+//   console.log("click2");
+// });
+// user.on("change", () => {
+//   console.log("change");
+// });
+
+// user.trigger("click");
--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,33 +1,20 @@
-interface userProps {
+import { Eventing } from "./Eventing";
+import { Sync } from "./Sync";
+import { Attributes } from "./Attributes";
+
+export interface UserProps {
+  id?: number;
   name?: string;
   age?: number;
 }
-type Callback = () => void;
 
 export class User {
-  events: { [key: string]: Callback[] } = {};
-  constructor(private data: userProps) {}
-  get(propName: string): number | string {
-    return this.data[propName];
-  }
-  set(update: userProps): void {
-    Object.assign(this.data, update);
-  }
-  on(eventName: string, callback: Callback) {
-    const handlers = this.events[eventName] || [];
-    handlers.push(callback);
-    this.events[eventName] = handlers;
-  }
-
-  trigger(eventName: string): void {
-    const handlers = this.events[eventName];
-
-    if (!handlers || handlers.length === 0) {
-      return;
-    }
-
-    handlers.forEach((callback) => {
-      callback();
-    });
+  public events: Eventing = new Eventing();
+  public sync: Sync<UserProps> = new Sync<UserProps>(
+    "http://localhost:3000/users"
+  );
+  public attributes: Attributes<UserProps>;
+  constructor(attrs: UserProps) {
+    this.attributes = new Attributes<UserProps>(attrs);
   }
 }
